caf18cf7e6a5c7ee0b42625436d2cdf9970a0377
DreamCatcher <dct328@163.com>
2018-09-23 22:43:17 +1000

    180923

--- Interpolation.py
+++ Interpolation.py
@@ -8,6 +8,15 @@ def LinearInterpolation2D(x1, y1, x2, y2, xi, tol=0.01):
         yi = k*(xi - x1) + y1
         return yi
 
+def IfColinear(x1, y1, x2, y2, x3, y3, tol=0.01):
+    vector1 = [x2 - x1, y2 - y1]
+    vector2 = [x3 - x1, y3 - y1]
+    pand = vector1[0]*vector2[1] - vector1[1]*vector2[0]
+    if abs(pand) < tol:
+        return True
+    else:
+        return False
+
 def LinearInterpolation3D(x1, y1, z1, x2, y2, z2, x3, y3, z3, xi, yi, tol=0.01):
     if abs(x1 - xi) < tol and abs(y1 - yi) < tol:
         return z1
@@ -16,7 +25,11 @@ def LinearInterpolation3D(x1, y1, z1, x2, y2, z2, x3, y3, z3, xi, yi, tol=0.01):
     elif abs(x3 - xi) < tol and abs(y3 - yi) < tol:
         return z3
     else:
-        return (x1*y2*z3 - x1*y3*z2 + x1*yi*z2 - x1*yi*z3 - x2*y1*z3 + x2*y3*z1 - x2*yi*z1 + x2*yi*z3 + x3*y1*z2 - x3*y2*z1 + x3*yi*z1 - x3*yi*z2 - xi*y1*z2 + xi*y1*z3 + xi*y2*z1 - xi*y2*z3 - xi*y3*z1 + xi*y3*z2)/(x1*y2 - x1*y3 - x2*y1 + x2*y3 + x3*y1 - x3*y2)
+        temp = x1*y2 - x1*y3 - x2*y1 + x2*y3 + x3*y1 - x3*y2
+        if not temp == 0:
+            return (x1*y2*z3 - x1*y3*z2 + x1*yi*z2 - x1*yi*z3 - x2*y1*z3 + x2*y3*z1 - x2*yi*z1 + x2*yi*z3 + x3*y1*z2 - x3*y2*z1 + x3*yi*z1 - x3*yi*z2 - xi*y1*z2 + xi*y1*z3 + xi*y2*z1 - xi*y2*z3 - xi*y3*z1 + xi*y3*z2)/(x1*y2 - x1*y3 - x2*y1 + x2*y3 + x3*y1 - x3*y2)
+        else:
+            print("Error, Three points are colinear")
 
 '''from sympy import *
 x1, y1, z1, x2, y2, z2, x3, y3, z3, xi, yi, zi = symbols('x1, y1, z1, x2, y2, z2, x3, y3, z3, xi, yi, zi')
@@ -33,3 +46,9 @@ print(zz)'''
 
 '''yi = LinearInterpolation3D(0, 0, 0, 1, 0, 1, 0, 1, 2, 0.5, 0.5)
 print(yi)'''
+
+y1 = LinearInterpolation2D(56326.3, 1.070235, 60366.4, 1.076009, 60000)
+print(y1)
+
+#yi = LinearInterpolation2D(88843.4, 1.072411, 93073.1, 1.080109, 1.10634898685)
+#print(yi)
---
+++ MomentDistributionMethodForContinuousBeam.py
@@ -0,0 +1,236 @@
+
+# =================================== Length of Each Span (From Left to Right) =================================
+Length = [8, 10, 6]
+
+# ================================== Young's Modulus of each span ==============================================
+E = [29000, 29000, 29000, 29000, 29000]
+
+# ================================== Moment of Inertia of each span ============================================
+I = [4860, 4860, 4860, 4860, 4860]
+
+# ================================== UDL of each span ==========================================================
+UDL = [0, -12.6, -20]
+
+# ================================== Concentrated Load (Act on mid-span) =======================================
+CL = [-100, 0, 0]
+
+# ================================== Support Type of Start and End =============================================
+# Available options are 'fix', 'pin', 'roller'
+SP = ['pin', 'fix']
+
+# ================================= Tolerance ==========================
+Tol = 0.01
+
+# ======================================================== BEGIN COMPUTATION ===================================
+
+class Element:
+    instances = []
+    def __init__(self, n, L, E, I, UDL, CL, start=False, end=False):
+        self.n = n
+        self.stiffness = float(E*I)/L
+        self.start = start
+        self.end = end
+        self.UDL = float(UDL)
+        self.CL = float(CL)
+        self.L = float(L)
+        self.distM1 = []
+        self.distM2 = []
+        self.CO1 = []
+        self.CO2 = []
+        if start:
+            if SP[0] == 'fix':
+                self.FEM1 = 1.0 / 12 * self.UDL * self.L * self.L + 1.0 / 8 * self.CL * self.L
+                self.FEM2 = -self.FEM1
+            elif SP[0] == 'pin':
+                self.FEM1 = 0
+                self.FEM2 = -1.0 / 8 * self.UDL * self.L * self.L - 3.0 / 16 * self.CL * self.L
+            elif SP[0] == 'roller':
+                self.FEM1 = - 1.0 / 6 * self.UDL * self.L * self.L - 1.0 / 8 * self.CL * self.L
+                self.FEM2 = - 1.0 / 3 * self.UDL * self.L * self.L - 3.0 / 8 * self.CL * self.L
+
+        elif end:
+            if SP[1] == 'fix':
+                self.FEM1 = 1.0 / 12 * self.UDL * self.L * self.L + 1.0 / 8 * self.CL * self.L
+                self.FEM2 = -self.FEM1
+            elif SP[1] == 'pin':
+                self.FEM1 = 1.0 / 8 * self.UDL * self.L * self.L - 3.0 / 16 * self.CL * self.L
+                self.FEM2 = 0
+            elif SP[1] == 'roller':
+                self.FEM1 = 1.0 / 3 * self.UDL * self.L * self.L - 1.0 / 8 * self.CL * self.L
+                self.FEM2 = 1.0 / 6 * self.UDL * self.L * self.L - 3.0 / 8 * self.CL * self.L
+
+        else:
+            self.FEM1 = 1.0 / 12 * self.UDL * self.L * self.L + 1.0 / 8 * self.CL * self.L
+            self.FEM2 = -self.FEM1
+
+
+        Element.instances.append(self)
+
+    def AddDistributionFactor1(self, df1):
+        self.df1 = df1
+
+    def AddDistributionFactor2(self, df2):
+        self.df2 = df2
+
+    def AddDistributionM1(self, M1):
+        self.distM1.append(M1)
+
+    def AddDistributionM2(self, M2):
+        self.distM2.append(M2)
+
+    def AddCO(self):
+        if self.start:
+            if SP[0] == 'pin':
+                self.CO1.append(0)
+            elif SP[0] == 'roller':
+                self.CO1.append(-self.distM2[-1])
+            elif SP[0] == 'fix':
+                self.CO1.append(self.distM2[-1]*0.5)
+        elif self.end:
+            if SP[1] == 'pin':
+                self.CO2.append(0)
+            elif SP[1] == 'roller':
+                self.CO2.append(-self.distM1[-1])
+            elif SP[1] == 'fix':
+                self.CO2.append(self.distM1[-1]*0.5)
+
+        else:
+            self.CO1.append(self.distM2[-1]*0.5)
+            self.CO2.append(self.distM1[-1]*0.5)
+
+    def FinalMoment(self):
+        self.FiM1 = 0
+        self.FiM2 = 0
+        self.FiM1 += self.FEM1
+        self.FiM1 += sum(self.distM1)
+        self.FiM1 += sum(self.CO1)
+        self.FiM2 += self.FEM2
+        self.FiM2 += sum(self.distM2)
+        self.FiM2 += sum(self.CO2)
+
+class Analysis:
+    def __init__(self):
+        # =================== Add Elements ===============================
+        EleNumber = len(Length)
+        for i in range(0, EleNumber):
+            if i == 0:
+                Element(i + 1, Length[i], E[i], I[i], UDL[i], CL[i], start=True)
+            elif i == EleNumber - 1:
+                Element(i + 1, Length[i], E[i], I[i], UDL[i], CL[i], end=True)
+            else:
+                Element(i + 1, Length[i], E[i], I[i], UDL[i], CL[i])
+
+        # =================== Calculate Distribution Factors ============
+
+        print("===================== Distribution Factors ===========================\n")
+        for i in range(0, EleNumber - 1):
+            k1 = Element.instances[i].stiffness
+            k2 = Element.instances[i + 1].stiffness
+            if i == 0:
+                if SP[0] == 'pin':
+                    k1 = 3*k1
+                    k2 = 4*k2
+                elif SP[0] == 'fix':
+                    k1 = 4*k1
+                    k2 = 4*k2
+            elif i == EleNumber - 2:
+                if SP[1] == 'pin':
+                    k2 = 3*k2
+                    k1 = 4*k1
+                elif SP[1] == 'fix':
+                    k2 = 4*k2
+                    k1 = 4*k1
+            else:
+                k1 = 4*k1
+                k2 = 4*k2
+
+            df1 = k1 / (k1 + k2)
+            df2 = k2 / (k1 + k2)
+            Element.instances[i].AddDistributionFactor2(df1)
+            Element.instances[i + 1].AddDistributionFactor1(df2)
+            print("Distribution Factor of Element %d at right side is %f" % (Element.instances[i].n, df1))
+            print("Distribution Factor of Element %d at left side is %f\n" % (Element.instances[i + 1].n, df2))
+
+
+        for i in range(0, EleNumber - 1):
+            Ele1 = Element.instances[i]
+            Ele2 = Element.instances[i + 1]
+            UBM = Ele1.FEM2 + Ele2.FEM1
+            Dist1 = -UBM*Ele1.df2
+            Dist2 = -UBM*Ele2.df1
+            Ele1.AddDistributionM2(Dist1)
+            Ele2.AddDistributionM1(Dist2)
+
+        for i in range(0, EleNumber):
+            Element.instances[i].AddCO()
+
+        # =======================================
+
+        for cycles in range(0, 2):
+            for i in range(0, EleNumber - 1):
+                Ele1 = Element.instances[i]
+                Ele2 = Element.instances[i + 1]
+                UBM = 0
+                if len(Ele1.CO2) > 0:
+                    UBM += Ele1.CO2[-1]
+                if len(Ele2.CO1) > 0:
+                    UBM += Ele2.CO1[-1]
+                Dist1 = -UBM * Ele1.df2
+                Dist2 = -UBM * Ele2.df1
+                Ele1.AddDistributionM2(Dist1)
+                Ele2.AddDistributionM1(Dist2)
+
+            for i in range(0, EleNumber):
+                Element.instances[i].AddCO()
+
+        # ======================================
+
+        for i in range(0, EleNumber - 1):
+            Ele1 = Element.instances[i]
+            Ele2 = Element.instances[i + 1]
+            UBM = 0
+            if len(Ele1.CO2) > 0:
+                UBM += Ele1.CO2[-1]
+            if len(Ele2.CO1) > 0:
+                UBM += Ele2.CO1[-1]
+            Dist1 = -UBM * Ele1.df2
+            Dist2 = -UBM * Ele2.df1
+            Ele1.AddDistributionM2(Dist1)
+            Ele2.AddDistributionM1(Dist2)
+
+
+        # ======================== Summation of Moments =============================
+
+        print("Results:")
+        for i in range(0, EleNumber):
+            Element.instances[i].FinalMoment()
+            print(Element.instances[i].FiM1)
+            print(Element.instances[i].FiM2)
+
+        # ========================= Show Process ====================================
+        print("Process:")
+        print("DF")
+        for i in range(0, EleNumber):
+            if i == 0:
+                print(Element.instances[i].df2)
+                print('\n')
+            elif i == EleNumber - 1:
+                print(Element.instances[i].df1)
+                print('\n')
+            else:
+                print(Element.instances[i].df1)
+                print(Element.instances[i].df2)
+                print('\n')
+
+        print("FEM")
+        for i in range(0, EleNumber):
+            print(-Element.instances[i].FEM1, -Element.instances[i].FEM2)
+
+        print('Dist')
+        for i in range(0, EleNumber):
+            print(Element.instances[i].distM1, Element.instances[i].distM2)
+
+        print('CO')
+        for i in range(0, EleNumber):
+            print(Element.instances[i].CO1, Element.instances[i].CO2)
+Analysis()
