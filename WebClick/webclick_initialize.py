6c7614a32673f6cc19963ae4a08ca815be4ad22e
ca954bj <ca954bj@gmail.com>
2021-03-11 16:22:30 +1100

    210311

--- WebClick/webclick_initialize.py
+++ WebClick/webclick_initialize.py
@@ -3,9 +3,9 @@ import time
 from selenium import webdriver
 
 class fwebdriver:
-	def _init_(self):
+	def __init__(self):
 		# Here Firefox will be used
-		driver = webdriver.Firefox()
+		self.driver = webdriver.Firefox()
 
 		# URL of website
 		#url = "https://www.geeksforgeeks.org/"
@@ -21,67 +21,74 @@ class fwebdriver:
 		# data-item-id missions_missionview_missionview_action_0
 
 		# Opening the website
-		driver.get(url)
+		self.driver.get(url)
 
 		# get the button by xpath
-		driver.find_element_by_xpath('''//*[@id="login_username"]''').send_keys("distributor")
-		driver.find_element_by_xpath('''//*[@id="login_password"]''').send_keys("distributor")
-		driver.find_element_by_xpath('''//*[@id="submit"]''').click()
+		self.driver.find_element_by_xpath('''//*[@id="login_username"]''').send_keys("distributor")
+		self.driver.find_element_by_xpath('''//*[@id="login_password"]''').send_keys("distributor")
+		self.driver.find_element_by_xpath('''//*[@id="submit"]''').click()
 		time.sleep(2)
 
 		print("click setup")
-		driver.find_element_by_xpath("/html/body/div[7]/div[1]/div[1]/ul/li[2]/div").click()
+		self.driver.find_element_by_xpath("/html/body/div[7]/div[1]/div[1]/ul/li[2]/div").click()
 		time.sleep(1)
 
 		print("click mission")
-		driver.find_element_by_xpath("/html/body/div[7]/div[1]/div[2]/div[2]/ul/li[1]/a/span").click()
+		self.driver.find_element_by_xpath("/html/body/div[7]/div[1]/div[2]/div[2]/ul/li[1]/a/span").click()
 		time.sleep(2)
 
 		print("edit trial")
-		driver.find_element_by_xpath("/html/body/div[7]/div[2]/div[2]/div[1]/div[2]/div[68]/div[5]").click()
+		self.driver.find_element_by_xpath("/html/body/div[7]/div[2]/div[2]/div[1]/div[2]/div[68]/div[5]").click()
 		time.sleep(1)
+		
+		try:
+			self.driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[1]").click()
+			time.sleep(1)
+		except:
+			pass
 
 		print("begin editing data")
-		driver.find_element_by_xpath("/html/body/div[7]/div[2]/div[2]/div[3]/ul/li/div/div[5]").click()
+		self.driver.find_element_by_xpath("/html/body/div[7]/div[2]/div[2]/div[3]/ul/li/div/div[5]").click()
 		time.sleep(1)
+		print("Initialized")
 
 	def edit_and_run(self, distancex, distancey, angle):
 		# here is the mission panel
 		t0 = time.clock()
 		# edit x
-		driver.find_element_by_name('''missionview_missionview_settingspanel_field_x''').clear()
+		self.driver.find_element_by_name('''missionview_missionview_settingspanel_field_x''').clear()
 		distancex = str(distancex)
-		driver.find_element_by_name('''missionview_missionview_settingspanel_field_x''').send_keys(distancex)
+		self.driver.find_element_by_name('''missionview_missionview_settingspanel_field_x''').send_keys(distancex)
 		
 		# edit y
-		driver.find_element_by_name('''missionview_missionview_settingspanel_field_y''').clear()
+		self.driver.find_element_by_name('''missionview_missionview_settingspanel_field_y''').clear()
 		distancey = str(distancey)
-		driver.find_element_by_name('''missionview_missionview_settingspanel_field_y''').send_keys(distancey)
+		self.driver.find_element_by_name('''missionview_missionview_settingspanel_field_y''').send_keys(distancey)
 		
 		# edit orientation
-		driver.find_element_by_name('''missionview_missionview_settingspanel_field_orientation''').clear()
+		self.driver.find_element_by_name('''missionview_missionview_settingspanel_field_orientation''').clear()
 		angle = str(angle)
-		driver.find_element_by_name('''missionview_missionview_settingspanel_field_orientation''').send_keys(angle)
+		self.driver.find_element_by_name('''missionview_missionview_settingspanel_field_orientation''').send_keys(angle)
 		
 		# save
-		driver.find_element_by_name("missionview_missionview_settingspanel_field_setbutton").click()
+		self.driver.find_element_by_name("missionview_missionview_settingspanel_field_setbutton").click()
 		time.sleep(0.5)
-		driver.find_element_by_xpath('''/html/body/div[7]/div[2]/div[2]/div[1]/div[2]/ul/li[2]/div''').click()
+		self.driver.find_element_by_xpath('''/html/body/div[7]/div[2]/div[2]/div[1]/div[2]/ul/li[2]/div''').click()
 		time.sleep(2)
-		driver.find_element_by_xpath("/html/body/div[7]/div[2]/div[2]/div[1]/div[2]/ul/li[1]/a").click()
+		self.driver.find_element_by_xpath("/html/body/div[7]/div[2]/div[2]/div[1]/div[2]/ul/li[1]/a").click()
 		time.sleep(2)
 		
 		# here is the mission panel
 		print("Run")
-		driver.find_element_by_xpath("/html/body/div[7]/div[2]/div[2]/div[1]/div[2]/div[68]/div[3]").click()
+		self.driver.find_element_by_xpath("/html/body/div[7]/div[2]/div[2]/div[1]/div[2]/div[68]/div[3]").click()
 		t1 = time.clock()
 		print("Time: %f" % (t1 - t0))
 		
 		print("edit trial")
-		driver.find_element_by_xpath("/html/body/div[7]/div[2]/div[2]/div[1]/div[2]/div[68]/div[5]").click()
+		self.driver.find_element_by_xpath("/html/body/div[7]/div[2]/div[2]/div[1]/div[2]/div[68]/div[5]").click()
 		time.sleep(1)
 
 		print("begin editing data")
-		driver.find_element_by_xpath("/html/body/div[7]/div[2]/div[2]/div[3]/ul/li/div/div[5]").click()
+		self.driver.find_element_by_xpath("/html/body/div[7]/div[2]/div[2]/div[3]/ul/li/div/div[5]").click()
 		time.sleep(1)
 
